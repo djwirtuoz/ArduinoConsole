493944fea7d075705645b6fb9368a865d903f42a
djwirtuoz <djwirtuoz@gmail.com>
2022-03-24 23:31:44 +0300

    Добавьте файлы проекта.

---
+++ ArduinoConsole/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ArduinoConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.найтиУстройствоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.включитьLEDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выключитьLEDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вывестиCOMportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.currentPort_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.connectButtonBtn = new System.Windows.Forms.Button();
+            this.closeConnectionBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.selectSpeedBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // найтиУстройствоToolStripMenuItem
+            // 
+            this.найтиУстройствоToolStripMenuItem.Name = "найтиУстройствоToolStripMenuItem";
+            this.найтиУстройствоToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // включитьLEDToolStripMenuItem
+            // 
+            this.включитьLEDToolStripMenuItem.Name = "включитьLEDToolStripMenuItem";
+            this.включитьLEDToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // выключитьLEDToolStripMenuItem
+            // 
+            this.выключитьLEDToolStripMenuItem.Name = "выключитьLEDToolStripMenuItem";
+            this.выключитьLEDToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // вывестиCOMportsToolStripMenuItem
+            // 
+            this.вывестиCOMportsToolStripMenuItem.Name = "вывестиCOMportsToolStripMenuItem";
+            this.вывестиCOMportsToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 371);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(652, 121);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вывод лога и ввод команд";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(571, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(559, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(640, 69);
+            this.listBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 26);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.clearLogToolStripMenuItem_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(652, 253);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Чат с устройством";
+            // 
+            // listBox2
+            // 
+            this.listBox2.ContextMenuStrip = this.contextMenuStrip2;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 18);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(640, 225);
+            this.listBox2.TabIndex = 0;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.очиститьToolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(133, 48);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Enabled = false;
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // очиститьToolStripMenuItem1
+            // 
+            this.очиститьToolStripMenuItem1.Name = "очиститьToolStripMenuItem1";
+            this.очиститьToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.очиститьToolStripMenuItem1.Text = "Очистить";
+            this.очиститьToolStripMenuItem1.Click += new System.EventHandler(this.clearChatToolStripMenuItem1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 535);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(677, 22);
+            this.statusStrip1.TabIndex = 8;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(114, 17);
+            this.toolStripStatusLabel1.Text = "Arduino Uno/COM3";
+            // 
+            // currentPort_menu
+            // 
+            this.currentPort_menu.Name = "currentPort_menu";
+            this.currentPort_menu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 8);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(652, 98);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Выбор порта";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(6, 19);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.ScrollAlwaysVisible = true;
+            this.listBox3.Size = new System.Drawing.Size(640, 69);
+            this.listBox3.TabIndex = 0;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // connectButtonBtn
+            // 
+            this.connectButtonBtn.Location = new System.Drawing.Point(12, 502);
+            this.connectButtonBtn.Name = "connectButtonBtn";
+            this.connectButtonBtn.Size = new System.Drawing.Size(95, 23);
+            this.connectButtonBtn.TabIndex = 10;
+            this.connectButtonBtn.Text = "Подключиться";
+            this.connectButtonBtn.UseVisualStyleBackColor = true;
+            this.connectButtonBtn.Click += new System.EventHandler(this.connectButtonBtn_Click);
+            // 
+            // closeConnectionBtn
+            // 
+            this.closeConnectionBtn.Location = new System.Drawing.Point(113, 502);
+            this.closeConnectionBtn.Name = "closeConnectionBtn";
+            this.closeConnectionBtn.Size = new System.Drawing.Size(94, 23);
+            this.closeConnectionBtn.TabIndex = 11;
+            this.closeConnectionBtn.Text = "Отключиться";
+            this.closeConnectionBtn.UseVisualStyleBackColor = true;
+            this.closeConnectionBtn.Click += new System.EventHandler(this.closeConnectionBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(583, 498);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(82, 23);
+            this.exitBtn.TabIndex = 13;
+            this.exitBtn.Text = "Выход";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // selectSpeedBox
+            // 
+            this.selectSpeedBox.FormattingEnabled = true;
+            this.selectSpeedBox.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200"});
+            this.selectSpeedBox.Location = new System.Drawing.Point(213, 503);
+            this.selectSpeedBox.Name = "selectSpeedBox";
+            this.selectSpeedBox.Size = new System.Drawing.Size(121, 21);
+            this.selectSpeedBox.TabIndex = 14;
+            this.selectSpeedBox.SelectedIndexChanged += new System.EventHandler(this.selectSpeedBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 557);
+            this.ControlBox = false;
+            this.Controls.Add(this.selectSpeedBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.closeConnectionBtn);
+            this.Controls.Add(this.connectButtonBtn);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "    Arduino console";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem включитьLEDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выключитьLEDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem найтиУстройствоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вывестиCOMportsToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip currentPort_menu;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button connectButtonBtn;
+        private System.Windows.Forms.Button closeConnectionBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.ComboBox selectSpeedBox;
+    }
+}
+
---
+++ ArduinoConsole/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Timers;
+
+namespace ArduinoConsole
+{
+    public partial class Form1 : Form
+    {
+        private delegate void updateDelegate(string txt);
+        public string currentPort;//выбранный порт в listbox
+        SerialPort openedPort;//открываемый порт
+        System.Timers.Timer aTimer;
+        int biteRate = 9600;
+        string[] ports = SerialPort.GetPortNames();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String dataOnwrite = textBox1.Text as String;
+                openedPort.Write(dataOnwrite);
+                listBox2.Items.Add("                                              " + dataOnwrite);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+            catch { listBox1.Items.Add("Подключение не установленно"); }
+        }//отправка данных
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)//чтение вошедших данных
+        {
+            try
+            {
+                if (openedPort.IsOpen)
+                {
+                    openedPort.DiscardInBuffer();
+                    String dataFromPort = openedPort.ReadLine();
+                    listBox2.BeginInvoke(new updateDelegate(updateTextBox), dataFromPort);
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void updateTextBox(string txt)
+        {
+            listBox2.Items.Add(txt);
+        }//обновление чата
+
+        private void clearLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }//меню ПКМ лога
+
+        private void clearChatToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }//меню по ПКМ чата
+
+        private void searchPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Length == 0)
+            {
+                toolStripStatusLabel1.Text = ("Активных COM портов не обнаружено");
+                connectButtonBtn.Enabled = false;
+                closeConnectionBtn.Enabled = false;
+                button1.Enabled = false;
+                listBox3.Items.Clear();
+            }
+            else if (ports.Length == 1)
+            {
+                toolStripStatusLabel1.Text = ("Найден COM порт, найденный порт выбран по умолчанию");
+                connectButtonBtn.Enabled = true;
+                button1.Enabled = true;
+                closeConnectionBtn.Enabled = false;
+                currentPort = ports[0];
+                listBox3.Items.Clear();
+                listBox3.Items.AddRange(ports);
+            }
+            else if (ports.Length > 1)
+            {
+                toolStripStatusLabel1.Text = ("Найдены COM порты");
+                listBox3.Items.Clear();
+                listBox3.Items.AddRange(ports);
+            }
+        }//поиск портов
+
+        private void connectButtonBtn_Click(object sender, EventArgs e)
+        {
+            foreach (string port in ports)
+            {
+                openedPort = new SerialPort(port, biteRate);
+                if (openedPort.IsOpen)
+                {
+                    MessageBox.Show("Opened port is allready opened", "WTF", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            openPortConnection();
+        }//подключение к порту, деуствие по кнопке
+
+        private void closeConnectionBtn_Click(object sender, EventArgs e)
+        {
+            closePortConnection();
+        }//закрытие подключения, действие по кнопке
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }//кнопка выхода
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа написана djwirtuoz, в 2017 году", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }//о программе
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectButtonBtn.Enabled = false;
+            closeConnectionBtn.Enabled = false;
+            button1.Enabled = false;
+
+            selectSpeedBox.Text = (biteRate.ToString());
+
+            searchPorts();//поиск портов при запуске
+        }//установка начальных параметров
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedPort = listBox3.SelectedItem.ToString();
+            currentPort = selectedPort;
+            //MessageBox.Show("" + selectedPort,"WTF", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            connectButtonBtn.Enabled = true;
+        }//заносит в переменную порта к которому нужно подключиться имя выделенного порта
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            const int WM_DeviceChange = 0x219; //что-то связанное с usb
+            const int DBT_DEVICEARRIVAL = 0x8000; //устройство подключено
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004; // устройство отключено
+
+            if (m.Msg == WM_DeviceChange)
+            {
+                if (m.WParam.ToInt32() == DBT_DEVICEARRIVAL)
+                {
+                    //новое usb подключено
+                    searchPorts();
+                }
+                if (m.WParam.ToInt32() == DBT_DEVICEREMOVECOMPLETE)
+                {
+                    // usb отключено
+                    searchPorts();
+                }
+
+            }
+        }//ловит системное событие на подключение устройства и запускает поиск портов
+
+        private void selectSpeedBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+              int biteRateSelected = Convert.ToInt32(selectSpeedBox.SelectedItem);
+                biteRate = biteRateSelected;
+            }
+            catch
+            {
+                MessageBox.Show("Какая то фигня в коде, по идее все должно работать", "WTF", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }//выбор скорости подключения
+
+        private void closePortConnection()
+        {
+            try
+            {
+                openedPort.Close();
+                listBox1.Items.Add("Подключение закрыто");
+                connectButtonBtn.Enabled = true;
+            }
+            catch
+            {
+                listBox1.Items.Add("Порт уже отключен");
+            }
+        }//закрытие подключения
+
+        private void openPortConnection()
+        {
+            openedPort = new SerialPort(currentPort, biteRate);
+            System.Threading.Thread.Sleep(500); // немного подождем
+
+            openedPort.BaudRate = biteRate;
+            openedPort.DtrEnable = true;
+            openedPort.ReadTimeout = 1000;
+            try
+            {
+                openedPort.Open();
+                System.Threading.Thread.Sleep(500);
+                listBox1.Items.Add("Подключенно к порту " + currentPort + ", на скорости " + biteRate.ToString());
+                aTimer = new System.Timers.Timer(500);
+                aTimer.Elapsed += OnTimedEvent;
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
+                closeConnectionBtn.Enabled = true;
+                button1.Enabled = true;
+            }
+            catch
+            {
+                listBox1.Items.Add("Не подключенно к порту" + currentPort);
+            }
+            connectButtonBtn.Enabled = false;
+        }//подключение к порту
+    }
+}
